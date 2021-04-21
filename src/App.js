376deac1e5b4170c7677e0bf19f6659cc80f4dac
Juan P. Montecinos <201400076@est.umss.edu>
2021-04-21 02:12:52 -0400

    home de la aplicacion

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import logo from './logo.svg';
+import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+}from 'react-router-dom';
+import IconLabelButtons from './componentes/IconLabelButtons';
+import BasicTextFields from './componentes/BasicTextFields';
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <h1>ComeCon</h1>
+        <div>
+          <Link to="/login">
+            login
+          </Link>
+          <Link to="/registrar">
+            registrar
+          </Link>
+        </div>
+        <Switch>
+        <Router path="/">
+          <BasicTextFields/>
+          <IconLabelButtons/>
+          </Router>
+          <Router path="/login">
+            login
+          </Router>
+          <Router path="/registrarse">
+            Registrar
+          </Router>
+        </Switch>
+      </div>
+    </Router>
+    
+  );
+}
+
+export default App;
---
+++ src/componentes/IconLabelButtons.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import { makeStyles } from '@material-ui/core/styles';
+import DeleteIcon from '@material-ui/icons/Delete';
+import CloudUploadIcon from '@material-ui/icons/CloudUpload';
+import KeyboardVoiceIcon from '@material-ui/icons/KeyboardVoice';
+import Icon from '@material-ui/core/Icon';
+import SaveIcon from '@material-ui/icons/Save';
+import SearchIcon from '@material-ui/icons/Search';
+
+const useStyles = makeStyles((theme) => ({
+  button: {
+    margin: theme.spacing(1),
+  },
+}));
+
+export default function IconLabelButtons() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Button
+        variant="contained"
+        color="primary"
+        className={classes.button}
+        startIcon={<SearchIcon/>}
+      >
+        Buscar
+      </Button>
+    </div>
+  );
+}

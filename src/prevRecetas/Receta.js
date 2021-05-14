6c8fa380314d91ad14d99b135dcbd0f58fe575a8
mitco874 <82779170+mitco874@users.noreply.github.com>
2021-05-14 13:43:48 -0400

    cambios en el formulario para guardar las url

--- src/prevRecetas/Receta.js
+++ src/prevRecetas/Receta.js
@@ -1,16 +1,15 @@
 import React, {
-    useEffect,
-    useState,
-  } from 'react';
-  import {db} from '../formularioRegistro/firebase';
+  useEffect,
+  useState,
+} from 'react';
+import {db} from '../formularioRegistro/firebase';
 
-  
 const Receta=()=> {
-  console.log("oooopoppoop");
-    const recetaEjemplo="pan";
-    const [ingredientesReceta,setIngredientesReceta]=useState([]);
-    const [detalleReceta, setDetalleReceta]=useState([]);
-    //formato de detalleReceta
+console.log("oooopoppoop");
+  const recetaEjemplo="pan";
+  const [ingredientesReceta,setIngredientesReceta]=useState([]);
+  const [detalleReceta, setDetalleReceta]=useState([]);
+  //formato de detalleReceta
 //tendras una receta [nombreReceta1,calorias1,descripcion1]
 //formato de ingredientesReceta
 //tendras sus ingredientes[{nombreIngrediente1,cantidad1,unidad1,nombreReceta}
@@ -18,33 +17,33 @@ const Receta=()=> {
 //{nombreIngrediente3,cantidad3,unidad3,nombreReceta}
 //{nombreIngrediente4,cantidad4,unidad4,nombreReceta}
 //]
-    useEffect(()=>{getDatosReceta()},[]);  
-    const getDatosReceta=async()=>{
-         var obj;
-         var lista=[];         
-        const consultaIngredientes=await db.collection("ingrediente-receta").where('nombreReceta','==',recetaEjemplo).get();
-        consultaIngredientes.forEach((doc) => { 
-            obj=doc.data();
-            obj.id=doc.id;
-            lista.push(obj);
-            console.log(lista);
-          })
-        setIngredientesReceta(lista);
-        var obj2;
-         var lista2=[];   
-        const consultaDetalleReceta=await db.collection("receta").where('camponombre','==',recetaEjemplo).get();
-        consultaDetalleReceta.forEach((doc) => { 
-            obj2=doc.data();
-            obj2.id=doc.id;
-            lista2.push(obj);
-            console.log(lista2);
-          })
-        setDetalleReceta(lista2);
-    }
-    return (
-      <div>
-               
-      </div>
-    );
+  useEffect(()=>{getDatosReceta()},[]);  
+  const getDatosReceta=async()=>{
+       var obj;
+       var lista=[];         
+      const consultaIngredientes=await db.collection("ingrediente-receta").where('nombreReceta','==',recetaEjemplo).get();
+      consultaIngredientes.forEach((doc) => { 
+          obj=doc.data();
+          obj.id=doc.id;
+          lista.push(obj);
+          console.log(lista);
+        })
+      setIngredientesReceta(lista);
+      var obj2;
+       var lista2=[];   
+      const consultaDetalleReceta=await db.collection("receta").where('camponombre','==',recetaEjemplo).get();
+      consultaDetalleReceta.forEach((doc) => { 
+          obj2=doc.data();
+          obj2.id=doc.id;
+          lista2.push(obj);
+          console.log(lista2);
+        })
+      setDetalleReceta(lista2);
   }
+  return (
+    <div>
+             
+    </div>
+  );
+}
 export default Receta;

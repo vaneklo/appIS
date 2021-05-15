1bb119efb498e105d561ff41b278096d5a87e353
Juan P. Montecinos <201400076@est.umss.edu>
2021-05-15 08:54:34 -0400

    configuraciones de firebase

--- src/formularioRegistro/firebase.js
+++ src/formularioRegistro/firebase.js
@@ -3,15 +3,23 @@ import 'firebase/firestore'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDM6kernnpRgESuYKwH9tyZHLNYLSjw9fA",
-  authDomain: "fireabase-recetas-saludables.firebaseapp.com",
-  projectId: "fireabase-recetas-saludables",
-  storageBucket: "fireabase-recetas-saludables.appspot.com",
-  messagingSenderId: "421864252074",
-  appId: "1:421864252074:web:39484e37a241d3b4b466d3"
+  apiKey: "AIzaSyBNY4ZFxYV8lG0BNM9IXFq19HAAuQ1me5M",
+  //apiKey: "AIzaSyBNY4ZFxYV8lG0BNM9IXFq19HAAuQ1me5M",
+  authDomain: "recetas-saludables-69ee9.firebaseapp.com",
+  //authDomain: "fireabase-recetas-saludables.firebaseapp.com",
+  projectId: "recetas-saludables-69ee9",
+ //projectId: "fireabase-recetas-saludables",
+  storageBucket: "recetas-saludables-69ee9.appspot.com",
+  //storageBucket: "fireabase-recetas-saludables.appspot.com",
+  messagingSenderId: "265049758167",
+  //messagingSenderId: "421864252074",
+  appId: "1:265049758167:web:4926c4523a49c439d21392"
+  //appId: "1:421864252074:web:39484e37a241d3b4b466d3"
 };
 // Initialize Firebase
 const fb=firebase.initializeApp(firebaseConfig);
 //const storage=firebase.storage();
 export const db=fb.firestore();
+
+
 

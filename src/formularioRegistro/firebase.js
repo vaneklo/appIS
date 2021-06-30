9408724c210d6fa0d24fb73e455c9777e7ad7e08
EinaSwain <dayanabarra99@gmail.com>
2021-06-29 23:23:27 -0400

    Cambios firebase

--- src/formularioRegistro/firebase.js
+++ src/formularioRegistro/firebase.js
@@ -3,12 +3,12 @@ import 'firebase/firestore'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDOAUbyEd5-NTHR2fuTd-PyUgnwd-dmvPE",
-  authDomain: "recetassaludablesfinal.firebaseapp.com",
-  projectId: "recetassaludablesfinal",
-  storageBucket: "recetassaludablesfinal.appspot.com",
-  messagingSenderId: "267659076058",
-  appId: "1:267659076058:web:3ba088ebb0b16116dde0e3"
+  apiKey: "AIzaSyCXAlNbHIpxR3NosQ96F-sIsLlnn_zctUc",
+    authDomain: "base-2b48c.firebaseapp.com",
+    projectId: "base-2b48c",
+    storageBucket: "base-2b48c.appspot.com",
+    messagingSenderId: "151076658992",
+    appId: "1:151076658992:web:225b9d86d9fdbe657d32f5"
 };
 // Initialize Firebase
 const fb=firebase.initializeApp(firebaseConfig);
